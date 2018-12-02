2c7f9c92c2343fcbc44f61114cb50c2fd556e87e
IgorgsTSI <45207382+IgorgsTSI@users.noreply.github.com>
2018-12-02 18:09:54 -0200

    Add files via upload

---
+++ galpon/logado/opcoesdequarto.php
@@ -0,0 +1,7 @@
+<?php
+	session_start();
+	require_once("servidor.php");
+	require_once("cabecalho.php");
+?>
+
+<h1>Quartos disponiveis</h1>
--- galpon/logado/reserva.php
+++ galpon/logado/reserva.php
@@ -3,13 +3,13 @@
 	require_once("servidor.php");
 	require_once("cabecalho.php");
 ?>
-    
-    <form action="cadastrar.php" method="get" accept-charset="utf-8">
-	Nome<br>
-	<input type="text" name="nome"><br>
-	Email<br>
-	<input type="text" name="email"><br>
-	Senha<br>	
-	<input type="password" name="senha"><br>
-	<input type="submit" name="enviar" value="Cadastrar">
-    </form>
+    <h1>Tipo de cama para o quarto</h1><br>
+  		<form method="post" action="opcoesdequarto.php">
+			<select name="tipo">
+				<option value="solteiro">Solteiro</option>
+				<option value="casal">Casal</option>
+				
+				
+			</select>
+				<input type="submit" value="Enviar" />
+		</form>
