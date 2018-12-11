6dc48ded8f272614237a5e6b8f0ce24b3c1c6d23
IgorgsTSI <45207382+IgorgsTSI@users.noreply.github.com>
2018-12-11 14:49:49 -0200

    Add files via upload

--- galpon2/logado/editacliente.php
+++ galpon2/logado/editacliente.php
@@ -3,16 +3,28 @@
     require_once("servidor.php");
     require_once("cabecalho.php");
 
-   
+  if (isset($_GET['cliente']) && !empty($_GET['cliente'])) {
+		$cliente=$_GET['cliente'];
+		$comando = "SELECT * FROM clientes WHERE cd_cliente='$cliente'";
+		$enviar=mysqli_query($conn, $comando);
+		$cliente=mysqli_fetch_assoc($enviar);
+		// var_dump($cliente);
+		// die();
+	}	
+	
+
+	
 ?>
 
-	<form action="editarclientes.php" method="get" accept-charset="utf-8">
+	<form action="../editarclientes.php" method="post" accept-charset="utf-8">
+	<input type="hidden" name="cd_cliente" value="<?php echo $cliente['cd_cliente']?>"><br>
 	Nome<br>
-	<input type="text" name="nome_cliente"><br>
+	<input type="text" name="nome_cliente" value="<?php echo $cliente['nome_cliente']?>"><br>
 	RG<br>
-	<input type="text" name="rg" maxlength="10"><br>
+	<input type="text" name="rg" value="<?php echo $cliente['rg']?>" maxlength="10"><br>
 	Fone<br>	
-	<input type="text" name="fone"><br>
-	<input type="submit" name="enviar" value="Cadastrar">
+	<input type="text" name="fone" value="<?php echo $cliente['fone']?>"><br>
+	<input type="submit" name="editar" value="Editar">
     </form>
-    <a href="index.php">Voltar</a>
+    
+   
--- galpon2/logado/editarclientes.php
+++ galpon2/logado/editarclientes.php
@@ -2,35 +2,19 @@
 session_start();
 require_once("servidor.php");
 
-	if(isset($_GET['enviar'])) {
-		$cliente=$_GET['cliente'];
 
-		if(!empty($_GET['rg'])){
-			$rg=$_GET['rg'];
-			
-		
-			$comando="UPDATE clientes SET rg='$rg' WHERE cd_cliente=$cliente";
-			$enviar=mysqli_query($conn, $comando);
-			
-		}
-		
-		if(!empty($_GET['fone']){
-			$fone=$_GET['fone'];
-			
+if(isset($_POST['cd_cliente']) && !empty($_POST['cd_cliente']) && isset($_POST['nome_cliente']) && !empty($_POST['nome_cliente']) && isset($_POST['rg']) && !empty($_POST['rg']) && isset($_POST['fone']) && !empty($_POST['fone'])){
+	$cd_cliente=$_POST['cd_cliente'];
+	$fone=$_POST['fone'];
+	$nome_cliente=$_POST['nome_cliente'];
+	$rg=$_POST['rg'];
 
-			$comando="UPDATE clientes SET fone='$fone' WHERE cd_cliente=$cliente";
-			$enviar=mysqli_query($conn, $comando);
-			
-		}
-		
-		if(!empty($_GET['nome_cliente'])){
-			$nome_cliente=$_GET['nome_cliente'];
-
-			$comando="UPDATE clientes SET nome_cliente='$nome_cliente' WHERE cd_cliente=$cliente";
-			$enviar=mysqli_query($conn, $comando);
-			
-		}
-		if($enviar>0) {
+		$comando="UPDATE clientes SET rg='$rg', fone='$fone', nome_cliente='$nome_cliente' WHERE cd_cliente='$cd_cliente'";
+		// echo $comando;
+		$enviar=mysqli_query($conn, $comando); 
+		// var_dump($enviar);
+		// die();
+	if($enviar) {
 				$_SESSION['mensagem']="Editado com sucesso";
 				header("location:listadeclientes.php");
 				exit;
@@ -39,9 +23,9 @@ require_once("servidor.php");
 				header("location:listadeclientes.php");
 				exit;
 			}
-	}
-	}else{
-		header("location:index.php");
-				exit;
+	
+	}else {
+		header("location:../index.php");
+		exit;
 	}
 ?>	
